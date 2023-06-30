e572fae6637c28d28dbcf8b6130717109b3961f6
Raise nil <raisulislamniloy47@gmail.com>
2023-06-30 13:15:42 +0600

    Update repo

---
+++ admin/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Order Website - Home Page</title>
+
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!-- menu start -->
+    <div class="menu">
+        Menu
+    </div>
+    <!-- menu end -->
+
+    <!-- main start -->
+    <div class="main-content">
+        main-content
+    </div>
+    <!-- main end -->
+
+    <!-- footer start -->
+    <div class="footer">
+        footer
+    </div>
+    <!-- footer end -->
+</body>
+</html>
