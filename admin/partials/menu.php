be0c7f9d6f2e8e669504b2c2ea513325e062adf2
Raise nil <raisulislamniloy47@gmail.com>
2023-06-30 14:16:08 +0600

    Update repo

--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -12,8 +12,8 @@
     <div class="menu text-center">
         <div class="wrapper">
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Admin</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
                 <li><a href="#">Category</a></li>
                 <li><a href="#">Food</a></li>
                 <li><a href="#">Order</a></li>
