cc16cc38f742cd5bc25453e5876de48336ca80d9
Raise nil <raisulislamniloy47@gmail.com>
2023-07-02 22:53:52 +0600

    fix link

---
+++ admin/manage-category.php
@@ -0,0 +1,17 @@
+<?php 
+    $pageTitle="Food Order Website - Manage-Category Page";
+    include("partials/menu.php");
+?>
+    <!-- category start -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Category</h1>
+
+            
+
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!-- category end -->
+
+<?php include("partials/footer.php")?>
--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,7 +1,7 @@
     <!-- footer start -->
     <div class="footer">
         <div class="wrapper">
-        <p class="text-center">All rights reservers. Developed by <a href="https://github.com/Raisenil" target="_blank">Raisenil</a> </p>
+        <p class="text-center">All rights reservers. Developed by <a href="https://github.com/Raisenil" target="_blank" rel="noopener">Raisenil</a> </p>
         </div>
     </div>
     <!-- footer end -->
