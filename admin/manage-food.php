23caf371e9a358a22f5201f28359e98bb5773542
Raise nil <raisulislamniloy47@gmail.com>
2023-07-02 23:00:41 +0600

    Manage order added

---
+++ admin/manage-food.php
@@ -0,0 +1,17 @@
+<?php 
+    $pageTitle="Food Order Website - Manage-Food Page";
+    include("partials/menu.php");
+?>
+    <!-- Food start -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Food</h1>
+
+            
+
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!-- Food end -->
+
+<?php include("partials/footer.php")?>
---
+++ admin/manage-order.php
@@ -0,0 +1,17 @@
+<?php 
+    $pageTitle="Food Order Website - Manage-Order Page";
+    include("partials/menu.php");
+?>
+    <!-- Order start -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Order</h1>
+
+            
+
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!-- Order end -->
+
+<?php include("partials/footer.php")?>
