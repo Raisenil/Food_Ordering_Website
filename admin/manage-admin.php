17fcf8696f487aaf89e683fc78defe07a7217914
Raise nil <raisulislamniloy47@gmail.com>
2023-07-02 23:56:39 +0600

    Update repo

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -7,6 +7,9 @@
         <div class="wrapper">
             <h1>Manage Admin</h1>
 
+            <!-- Button to add Admin -->
+            <a href="#" class="btn-primary">Add Admin</a>
+
             <table class="tbl-full">
                 <tr>
                     <th>S.N.</th>
@@ -17,6 +20,33 @@
 
                 <tr>
                     <td>1.</td>
+                    <td>Raise nil</td>
+                    <td>Raise</td>
+                    <td>
+                        Update Admin
+                        Delete Admin
+                    </td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>Raise nil</td>
+                    <td>Raise</td>
+                    <td>
+                        Update Admin
+                        Delete Admin
+                    </td>
+                </tr>
+                <tr>
+                    <td>3.</td>
+                    <td>Raise nil</td>
+                    <td>Raise</td>
+                    <td>
+                        Update Admin
+                        Delete Admin
+                    </td>
+                </tr>
+                <tr>
+                    <td>4.</td>
                     <td>Raise nil</td>
                     <td>Raise</td>
                     <td>
