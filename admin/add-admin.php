a956a4d7232594bb3da4c2d6df1005a8c063fcf9
Raise nil <raisulislamniloy47@gmail.com>
2023-07-04 21:20:36 +0600

    DB config file added

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -52,10 +52,6 @@
             password = '$password'
             ";
 
-            $conn = mysqli_connect('localhost','root','') or die(mysqli_error());
-
-            $db_select = mysqli_select_db($conn,'food_order') or die(mysqli_error());
-
             // $res = mysqli_query($conn,$sql) or die(mysqli_error());
     }
 ?>
