3e7809f7690b34df1184c9586ea9d4ce8e0eb3cf
Raise nil <raisulislamniloy47@gmail.com>
2023-07-04 21:04:45 +0600

    Update repo

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -53,6 +53,9 @@
             ";
 
             $conn = mysqli_connect('localhost','root','') or die(mysqli_error());
-            $res = mysqli_query($conn,$sql) or die(mysqli_error());
+
+            $db_select = mysqli_select_db($conn,'food_order') or die(mysqli_error());
+
+            // $res = mysqli_query($conn,$sql) or die(mysqli_error());
     }
 ?>
