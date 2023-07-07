d85ab63d9463af559c3368459448c0eed04ebb4a
Raise nil <raisulislamniloy47@gmail.com>
2023-07-08 02:40:19 +0600

    Update repo

--- admin/update-admin.php
+++ admin/update-admin.php
@@ -6,9 +6,44 @@
     <div class="main-content">
         <div class="wrapper">
             <h1>Update Admin</h1>
+            <br><br>
+
+            <?php 
+                $id=$_GET['id'];
+
+                $sql="SELECT * FROM tbl_admin:";
+
+                $res=mysqli_query($conn, $sql);
+
+                if($res==true){
+                    $count = mysqli_num_rows($res);
+
+                    if($count==1){
+                        echo "Admin Available"
+                    }else{
+                        header('location:'.SITEURL.'admin/manage-admin.php');
+                    }
+                }
+            ?>
 
             <form action="" method="POST">
-                
+                <table class="tbl-30">
+                    <tr>
+                        <td>Full Name:</td>
+                        <td><input type="text" name="full_name" value=" " required></td>
+                    </tr>
+
+                    <tr>
+                        <td>Username:</td>
+                        <td><input type="text" name="username" value="" required></td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="2">
+                            <input type="submit" name="submit" value="Update Admin" class="btn-secondary">
+                        </td>
+                    </tr>
+                </table>
             </form>
         </div>
     </div>
