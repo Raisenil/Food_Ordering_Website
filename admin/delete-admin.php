14befbb0010fba3d7bc16c75f91366103ed95ea6
Raise nil <raisulislamniloy47@gmail.com>
2023-07-08 00:35:24 +0600

    Update repo

--- admin/delete-admin.php
+++ admin/delete-admin.php
@@ -10,10 +10,10 @@
     if($res == true){
         // echo "Admin Deleted";
         $_SESSION['delete'] = "Admin Deleted Successfully";
-        header('localhost:'.SITEURL.'admin/manage-admin.php');
+        header('location:'.SITEURL.'admin/manage-admin.php');
     }else{
         // echo "Failed ro Delete Admin";
         $_SESSION['delete'] = "Failed to Delete Admin. Try Again Later.";
-        header('localhost:'.SITEURL.'admin/manage-admin.php');
+        header('location:'.SITEURL.'admin/manage-admin.php');
     }
 ?>
