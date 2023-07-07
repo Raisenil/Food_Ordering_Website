ed0aeebe1b584dcd57e0d9b559e39b7d9268787d
Raise nil <raisulislamniloy47@gmail.com>
2023-07-08 04:04:20 +0600

    Update repo

--- admin/update-password.php
+++ admin/update-password.php
@@ -8,22 +8,31 @@
         <h1>Change Password</h1>
         <br><br>
 
+        <?php 
+            if(isset($_GET['id'])){
+                $id=$_GET['id'];
+            }
+        ?>
         <form action="" method="POST">
             <table class="tbl-30">
                 <tr>
-                    <td>Full Name:</td>
-                    <td><input type="text" name="full_name" value="<?php echo $full_name; ?>" required></td>
+                    <td>Current Password: </td>
+                    <td><input type="password" name="current_password" placeholder="Old Password" required></td>
                 </tr>
 
                 <tr>
-                    <td>Username:</td>
-                    <td><input type="text" name="username" value="<?php echo $username; ?>" required></td>
+                    <td>New Password: </td>
+                    <td><input type="password" name="new_password" placeholder="New Password" required></td>
+                </tr>
+
+                <tr>
+                    <td>Confirm Password: </td>
+                    <td><input type="password" name="confirm_password" placeholder="Confirm Password" required></td>
                 </tr>
 
                 <tr>
                     <td colspan="2">
-                        <input type="hidden" name="id" value="<?php echo $id;?>">
-                        <input type="submit" name="submit" value="Update Admin" class="btn-secondary">
+                        <input type="submit" name="submit" value="change password">
                     </td>
                 </tr>
             </table>
