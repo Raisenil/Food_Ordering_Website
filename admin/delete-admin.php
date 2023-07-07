a8f98ae9ec409461ae23e85d9777111db2a7baec
Raise nil <raisulislamniloy47@gmail.com>
2023-07-08 00:15:18 +0600

    Update repo

--- admin/delete-admin.php
+++ admin/delete-admin.php
@@ -8,8 +8,12 @@
     $res = mysqli_query($conn, $sql);
 
     if($res == true){
-
+        // echo "Admin Deleted";
+        $_SESSION['delete'] = "Admin Deleted Successfully";
+        header('localhost:'.SITEURL.'admin/manage-admin.php');
     }else{
-        
+        // echo "Failed ro Delete Admin";
+        $_SESSION['delete'] = "Failed to Delete Admin. Try Again Later.";
+        header('localhost:'.SITEURL.'admin/manage-admin.php');
     }
 ?>
