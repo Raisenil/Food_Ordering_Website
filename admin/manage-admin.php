a435c0c3d41c52cd23c9e615d815295982e4e618
Raise nil <raisulislamniloy47@gmail.com>
2023-07-08 02:30:16 +0600

    Update repo

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -56,7 +56,7 @@
                                 <td><?php echo $full_name?></td>
                                 <td><?php echo $username?></td>
                                 <td>
-                                    <a href="#" class="btn-secondary">Update Admin</a>
+                                    <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>" class="btn-secondary">Update Admin</a>
                                     <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-red">Delete Admin</a>
                                 </td>
                             </tr>
--- admin/update-admin.php
+++ admin/update-admin.php
@@ -3,6 +3,15 @@
     include("partials/menu.php");
 ?>
 
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Update Admin</h1>
+
+            <form action="" method="POST">
+                
+            </form>
+        </div>
+    </div>
 
 
 <?php include("partials/footer.php");?>
