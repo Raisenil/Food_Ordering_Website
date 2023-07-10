cd811c75fdf525f801aacfd504b57036e11dba60
Raise nil <raisulislamniloy47@gmail.com>
2023-07-11 01:44:11 +0600

    Database updated

--- Database/food_order.sql
+++ Database/food_order.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 08, 2023 at 10:20 PM
+-- Generation Time: Jul 10, 2023 at 09:43 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -39,12 +39,9 @@ CREATE TABLE `tbl_admin` (
 --
 
 INSERT INTO `tbl_admin` (`id`, `full_name`, `username`, `password`) VALUES
-(1, 'Wing Guy', 'hojahalygo', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
 (11, 'Brenda Turner', 'fucezuh', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
 (15, 'Darryl Carney', 'levifufepe', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
-(16, 'Maggy Henry', 'pixexivaw', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
 (18, 'Kareem Fuller', 'pofyl', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
-(19, 'Arsenio Burke', 'sehigeryc', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
 (24, 'MD. Raisul Islam Niloy', 'Raisenil', 'ce9a94c2bbbd8812d28654cf51a67865'),
 (25, 'Nayda Mccullough', 'tacuq', 'f3ed11bbdb94fd9ebdefbaf646ab94d3');
 
@@ -67,7 +64,7 @@ CREATE TABLE `tbl_category` (
 --
 
 INSERT INTO `tbl_category` (`id`, `title`, `image_name`, `featured`, `active`) VALUES
-(1, 'Pizza', '', 'Yes', 'Yes');
+(2, 'Test Image', 'Web Design UI.png', 'Yes', 'Yes');
 
 -- --------------------------------------------------------
 
@@ -148,7 +145,7 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT for table `tbl_category`
 --
 ALTER TABLE `tbl_category`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tbl_food`
