44baf72e7f6286f42effede03b1d21f90c425e11
Raise nil <raisulislamniloy47@gmail.com>
2023-08-07 15:14:17 +0600

    Database backup

--- Food_Ordering_Website_Full/Database/food_order.sql
+++ Food_Ordering_Website_Full/Database/food_order.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2023 at 10:45 PM
+-- Generation Time: Aug 07, 2023 at 11:13 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
